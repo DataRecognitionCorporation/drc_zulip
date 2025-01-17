43a4a376561fbb9656948a707ad60aee371489f4
asher-tormanen <72164075+asher-tormanen@users.noreply.github.com>
2025-01-16 21:54:49 -0600

    notask/buddy-list (#82)
    * Buddy list will show all users in streams. Not just active users.

--- zerver/tornado/views.py
+++ zerver/tornado/views.py
@@ -117,7 +117,6 @@ def get_events_internal(
     assert is_current_port(get_user_tornado_port(user_profile))
 
     process_client(request, user_profile, client_name="internal")
-    print(user_profile)
     return get_events_backend(request, user_profile)
 
 
