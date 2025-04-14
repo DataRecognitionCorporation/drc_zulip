5a57e4d9a6d1eda4f207d4bf29d918ecff38ff9b
Asher Tormanen <atormanen@datarecognitioncorp.com>
2025-04-14 09:37:34 -0500

    update

--- zproject/urls.py
+++ zproject/urls.py
@@ -619,8 +619,9 @@ i18n_urls = [
     # Login/registration
     path("register/", accounts_home, name="register"),
 
-    path("login/", login_page, {"template_name": "zerver/login_drc.html"}, name="login_page"),
-    path("login_hidden/", login_page, {"template_name": "zerver/login.html"}, name="login_page"),
+    path("login/", login_page, {"template_name": "zerver/login.html"}, name="login_page"),
+    # path("login/", login_page, {"template_name": "zerver/login_drc.html"}, name="login_page"),
+    # path("login_hidden/", login_page, {"template_name": "zerver/login.html"}, name="login_page"),
 
     path("join/<confirmation_key>/", accounts_home_from_multiuse_invite, name="join"),
     # Used to generate a Zoom video call URL
