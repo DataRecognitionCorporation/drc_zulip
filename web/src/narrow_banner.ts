zulip/zulip

narrow_banner: Change banner text for empty `is:resolved` narrow.

web/src/narrow_banner.ts
@@ -298,7 +298,18 @@ export function pick_empty_narrow_banner(current_filter: Filter): NarrowBannerDa
                     };
                 case "resolved":
                     return {
-                        title: $t({defaultMessage: "No topics are marked as resolved."}),
+                        title_html: $t_html(
+                            {
+                                defaultMessage:
+                                    "No topics in <z-link>your history</z-link> are marked as resolved.",
+                            },
+                            {
+                                "z-link": (content_html) =>
+                                    `<a href="/help/search-for-messages#searching-shared-history" target="_blank" rel="noopener noreferrer">${content_html.join(
+                                        "",
+                                    )}</a>`,
+                            },
+                        ),
                     };
                 case "followed":
                     return {

web/tests/message_view.test.cjs
@@ -398,7 +398,10 @@ run_test("show_empty_narrow_message", ({mock_template, override, override_rewire
     narrow_banner.show_empty_narrow_message(current_filter);
     assert.equal(
         $(".empty_feed_notice_main").html(),
-        empty_narrow_html("translated: No topics are marked as resolved."),
+        empty_narrow_html(
+            undefined,
+            `translated: No topics in <a href="/help/search-for-messages#searching-shared-history" target="_blank" rel="noopener noreferrer">your history</a> are marked as resolved.`,
+        ),
     );
 
     current_filter = set_filter([["is", "followed"]]);

DataRecognitionCorporation/drc_zulip

web/src/narrow_banner.ts
@@ -320,7 +320,18 @@ export function pick_empty_narrow_banner(current_filter: Filter): NarrowBannerDa
                     };
                 case "resolved":
                     return {
-                        title: $t({defaultMessage: "No topics are marked as resolved."}),
+                        title_html: $t_html(
+                            {
+                                defaultMessage:
+                                    "No topics in <z-link>your history</z-link> are marked as resolved.",
+                            },
+                            {
+                                "z-link": (content_html) =>
+                                    `<a href="/help/search-for-messages#search-shared-history" target="_blank" rel="noopener noreferrer">${content_html.join(
+                                        "",
+                                    )}</a>`,
+                            },
+                        ),
                     };
                 case "followed":
                     return {

web/tests/message_view.test.cjs
@@ -403,7 +403,10 @@ run_test("show_empty_narrow_message", ({mock_template, override, override_rewire
     narrow_banner.show_empty_narrow_message(current_filter);
     assert.equal(
         $(".empty_feed_notice_main").html(),
-        empty_narrow_html("translated: No topics are marked as resolved."),
+        empty_narrow_html(
+            undefined,
+            `translated: No topics in <a href="/help/search-for-messages#search-shared-history" target="_blank" rel="noopener noreferrer">your history</a> are marked as resolved.`,
+        ),
     );
 
     current_filter = set_filter([["is", "followed"]]);